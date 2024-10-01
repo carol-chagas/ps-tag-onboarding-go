1e0b35940a0441eda4174957b52bda8a44874dc1
carol-chagas <anacarolina.daschagas@wexinc.com>
2024-10-01 10:07:55 -0300

    [new] adding email and age validation

--- internal/service/service.go
+++ internal/service/service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"errors"
+	"regexp"
 
 	"ps-tag-onboarding-go/internal/domain"
 	"ps-tag-onboarding-go/internal/repository"
@@ -15,7 +16,30 @@ func NewUserService(repo repository.UserRepository) *UserService {
 	return &UserService{repo: repo}
 }
 
+func (s *UserService) validateUser(user domain.User) error {
+	emailRegex := `^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$`
+	re := regexp.MustCompile(emailRegex)
+	if !re.MatchString(user.Email) {
+		return errors.New("invalid email")
+	}
+
+	if user.Age < 18 {
+		return errors.New("invalid age")
+	}
+
+	return nil
+}
+
 func (s *UserService) SaveUser(user domain.User) error {
+	if err := s.validateUser(user); err != nil {
+		return err
+	}
+
+	existingUser, _ := s.repo.FindByName(user.FirstName, user.LastName)
+	if existingUser.ID != "" && existingUser.ID != user.ID {
+		return errors.New("user already exists")
+	}
+
 	return s.repo.Save(user)
 }
 
